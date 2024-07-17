bdfa945c9287d71821fd1c36fcb14669b50dbab1
Sunil-Chakraborty <92195193+Sunil-Chakraborty@users.noreply.github.com>
2024-07-17 20:33:32 +0530

    Update settings.py

--- crudexample/settings.py
+++ crudexample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sj%0jx8n2esflir+g0yhtw(=bwvpnq)%3q2xpo6m=u!^)pcz##'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.190.120']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
