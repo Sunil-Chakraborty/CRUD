7b802a953faa87553dedc893726d940bb90844b6
Sunil-Chakraborty <92195193+Sunil-Chakraborty@users.noreply.github.com>
2024-07-20 18:56:26 +0530

    Update wsgi.py

--- crudexample/wsgi.py
+++ crudexample/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 import sys
 
-path = '/home/sunilc/CRUD/CRUD'
+path = '/home/sunilc/CRUD'
 if path not in sys.path:
     sys.path.append(path)
 
