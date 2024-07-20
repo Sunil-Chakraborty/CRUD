e4b11768b7486d680dacdd59575b9103753276d7
Sunil-Chakraborty <92195193+Sunil-Chakraborty@users.noreply.github.com>
2024-07-20 21:10:50 +0530

    Update wsgi.py

--- crudexample/wsgi.py
+++ crudexample/wsgi.py
@@ -7,11 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+
 import os
 import sys
 
+path = '/home/sunilc/CRUD'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'crudexample.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudexample.settings')
-
 application = get_wsgi_application()
