9841060cbf1ef0207d319b290549003364d4b659
Sunil-Chakraborty <92195193+Sunil-Chakraborty@users.noreply.github.com>
2024-07-20 21:06:47 +0530

    Update wsgi.py

--- crudexample/wsgi.py
+++ crudexample/wsgi.py
@@ -10,17 +10,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 import sys
 
-# Add the project directory to the sys.path
-sys.path.append('/home/sunilc/CRUD')
-sys.path.append('/home/sunilc/CRUD/crudexample')
-
-# Set the settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'crudexample.settings'
-
-# Activate the virtual environment
-activate_env = os.path.expanduser("/home/sunilc/CRUD/myenv/bin/activate_this.py")
-with open(activate_env) as f:
-    exec(f.read(), {'__file__': activate_env})
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudexample.settings')
+
 application = get_wsgi_application()
