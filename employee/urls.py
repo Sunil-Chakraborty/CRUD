4c5ea759e9957819797fa73ae5f10a6d5b046114
Sunil-Chakraborty <92195193+Sunil-Chakraborty@users.noreply.github.com>
2024-07-21 10:07:40 +0530

    Update urls.py

--- employee/urls.py
+++ employee/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
 from .import views
 
+urlpatterns = [
+    path('', views.show, name='show'),
+    path('show/', views.show, name='show'),
+    path('showall/', views.showall, name='showall'),
+    path('search/', views.search, name='search'),
+    path('doc/<int:id>', views.doc, name='doc'),
+    path('emp/', views.emp, name='emp'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.destroy, name='destroy'),
+]
+
+"""
 urlpatterns = [    
     path('', views.show, name='show'),
     path('search/', views.search, name='search'),    
     path('doc/', views.doc, name='doc'),
 ]
+"""
