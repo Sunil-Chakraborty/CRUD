3a0dec24da03a99ca1a8659d104677b9f50afb9b
Sunil-Chakraborty <92195193+Sunil-Chakraborty@users.noreply.github.com>
2024-07-22 22:11:05 +0530

    Update utils.py

--- employee/utils.py
+++ employee/utils.py
@@ -1,16 +1,24 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
-from xhtml2pdf import pisa
 
+
+#from xhtml2pdf import pisa
+
+try:
+    from xhtml2pdf import pisa
+    # from bidi.algorithm import get_display
+except ImportError:
+    pisa = None
 
 def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html = template.render(context_dict)
     response = HttpResponse(content_type='application/pdf')
-   
+
     pdf_status = pisa.CreatePDF(html, dest=response)
 
     if pdf_status.err:
         return HttpResponse('Some errors were encountered <pre>' + html + '</pre>')
 
     return response
+    
